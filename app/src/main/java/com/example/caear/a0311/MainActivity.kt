0132340726f6e6c114fb7711c6a283575239e763
arevalocarloseduardo <33073389+arevalocarloseduardo@users.noreply.github.com>
2017-11-03 19:40:31 -0300

    hola

--- app/src/main/java/com/example/caear/a0311/MainActivity.kt
+++ app/src/main/java/com/example/caear/a0311/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var yt = 0
+        print(yt)
     }
 }
